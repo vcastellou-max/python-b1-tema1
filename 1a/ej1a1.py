a1f1a2027eb2f83bdb432edbd6565920f9dc3fd6
vcastellou-max <vcastellou@uoc.edu>
2025-12-17 17:16:54 +0100

    Update ej1a1.py

--- 1a/ej1a1.py
+++ 1a/ej1a1.py
@@ -40,8 +40,17 @@ Exemple:
 '''
 
 def fibonacci(fibonacci_number):
+    if not isinstance(fibonacci_number,int):
+        raise ValueError("Number must be integer.")
+    elif fibonacci_number<0:
+        raise ValueError("Number must be greater than 0.")
+    else:
+        a,b=0,1
+    for _ in range(fibonacci_number):
+        a,b = b, a+b
+    return a
     # Write here your code
-    pass
+    
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
